a27bc3d90eb4a1c4d3c1bc94c71cce487deb8cfc
Xin <xinhong3@illinois.edu>
2021-04-14 17:51:25 -0500

    notification demo

--- app/src/main/java/com/example/vasistartapp/MainActivity.java
+++ app/src/main/java/com/example/vasistartapp/MainActivity.java
@@ -1,14 +1,40 @@
 package com.example.vasistartapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
+import android.app.Notification;
+import android.app.NotificationManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    private NotificationManagerCompat notificationManager;
+    private EditText editTextLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        notificationManager = NotificationManagerCompat.from(this);
+
+        editTextLocation = findViewById(R.id.location_text_input);
+    }
+
+    public void pushNotification(View view) {
+        String location = editTextLocation.getText().toString();
+        String title = "Your Car is Moving in " + location;
+        String message = "Click to check the vecicle location.";
+        Notification notification = new NotificationCompat.Builder(this, App.CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_android_black_24dp)
+                .setContentTitle(title)
+                .setContentText(message)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setCategory(NotificationCompat.CATEGORY_ALARM)
+                .build();
+        notificationManager.notify(1, notification);
     }
 }
