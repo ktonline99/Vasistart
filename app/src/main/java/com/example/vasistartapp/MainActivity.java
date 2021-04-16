9a63e895fac1cac179d666815a13445cb3c25b17
dcc3 <dcc3@illinois.edu>
2021-04-16 14:06:13 -0500

    Initial screen

--- app/src/main/java/com/example/vasistartapp/MainActivity.java
+++ app/src/main/java/com/example/vasistartapp/MainActivity.java
@@ -1,7 +1,8 @@
 package com.example.vasistartapp;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.view.View;
+import android.widget.TextView;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +11,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    int temp = 60;
+    public void decreaseTemp(android.view.View v){
+        TextView t = (TextView) v;
+        if(temp > 60){
+            t.setText(temp - 1);
+        }
+    }
+
+    public void increaseTemp(android.view.View v){
+        TextView t = (TextView) v;
+        if(temp < 90){
+            t.setText(temp + 1);
+        }
+
     }
 }
