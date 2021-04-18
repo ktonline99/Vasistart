78df60f11f290dc662a4b90205ce105de9d100d2
Andrew Yoo <abyoo2@illinois.edu>
2021-04-18 15:10:21 -0700

    updated settings page

--- app/src/main/java/com/example/vasistartapp/Settings.java
+++ app/src/main/java/com/example/vasistartapp/Settings.java
@@ -3,6 +3,11 @@ package com.example.vasistartapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.AdapterView;
+import android.widget.Spinner;
+import android.widget.ArrayAdapter;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Settings extends AppCompatActivity {
 
