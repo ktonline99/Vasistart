e1313a41bf9975518820311713f35bf9153167d4
Andrew Yoo <abyoo2@illinois.edu>
2021-04-20 16:25:43 -0700

    fixed some things

--- app/src/main/java/com/example/vasistartapp/myvehicle.java
+++ app/src/main/java/com/example/vasistartapp/myvehicle.java
@@ -3,13 +3,14 @@ package com.example.vasistartapp;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.collection.ArraySet;
-import androidx.preference.PreferenceManager;
+//import androidx.preference.PreferenceManager;
 
 import android.annotation.SuppressLint;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
