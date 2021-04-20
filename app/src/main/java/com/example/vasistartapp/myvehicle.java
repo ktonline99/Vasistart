f5753cdf456380c1bc5283a756f92f79db949d19
mollytyy <moty0519@gmail.com>
2021-04-19 20:56:34 -0500

    needs fix (in my vehicle?)

--- app/src/main/java/com/example/vasistartapp/myvehicle.java
+++ app/src/main/java/com/example/vasistartapp/myvehicle.java
@@ -1,14 +1,28 @@
 package com.example.vasistartapp;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.collection.ArraySet;
+import androidx.preference.PreferenceManager;
 
+import android.annotation.SuppressLint;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+
 
 public class myvehicle extends AppCompatActivity {
 
+    @SuppressLint("ResourceType")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,19 +30,44 @@ public class myvehicle extends AppCompatActivity {
 
         Button pair_another = (Button) findViewById(R.id.pair_another);
         Button set_default = (Button) findViewById(R.id.set_default);
+        ArrayList<String> list = new ArrayList<String>();
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this, R.id.listv);
+        ListView listView;
 
-        pair_another.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick (View v){
-                Intent intent = new Intent(v.getContext(), start.class);
-                v.getContext().startActivity(intent);}
-        });
+        SharedPreferences data = PreferenceManager.getDefaultSharedPreferences(this);
+        String dataSet = data.getString("car", "");
+
+        list.add(dataSet);
+        arrayAdapter.notifyDataSetChanged();
+
+        listView=(ListView)findViewById(R.id.listv);
+        arrayAdapter = new ArrayAdapter<String>(this, R.id.listv, list);
+        listView.setAdapter(arrayAdapter);
+
 
         set_default.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick (View v){
-                Intent intent = new Intent(v.getContext(), default_vehicle.class);
-                v.getContext().startActivity(intent);}
+                AlertDialog.Builder builder;
+                builder = new AlertDialog.Builder(getApplicationContext());
+                builder.setTitle("Select a default vehicle");
+
+                SharedPreferences sharedPreferences = getPreferences(MODE_PRIVATE);
+                Set<String> vehicles = sharedPreferences.getStringSet("car", new HashSet<String>());
+                CharSequence[] temp_vehicles = vehicles.toArray(new CharSequence[0]);
+                builder.setItems(temp_vehicles, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        list.add(0, (String) vehicles.toArray()[which]);
+                    }
+                });
+                builder.show();
+            }
         });
-    }
+        }
+
+    public void pairAnother (View v){
+        Intent intent = new Intent(v.getContext(), start.class);
+        v.getContext().startActivity(intent);}
 }
+
---
+++ app/src/main/java/com/example/vasistartapp/settings.java
@@ -0,0 +1,42 @@
+package com.example.vasistartapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.AdapterView;
+import android.widget.Spinner;
+import android.widget.ArrayAdapter;
+import android.widget.Switch;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class settings extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+
+        Spinner temp_spinner = (Spinner) findViewById(R.id.temperature_spinner);
+        Spinner unit_spinner = (Spinner) findViewById(R.id.units_spinner);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.temp_arrays, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        temp_spinner.setAdapter(adapter);
+
+        ArrayAdapter<CharSequence> unit_adapter = ArrayAdapter.createFromResource(this,
+                R.array.unit_arrays, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        unit_spinner.setAdapter(unit_adapter);
+
+        //TODO actually change the temperature
+
+        Switch moveNotifSwitch = (Switch) findViewById(R.id.moveNotifSwitch);
+        Switch lockNotifSwitch = (Switch) findViewById(R.id.lockNotifSwitch);
+
+        //TODO actually change the notifications
+
+    }
+}
