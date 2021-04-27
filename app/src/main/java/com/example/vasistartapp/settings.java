7f42bb641ae7352ce4d7023f489abdf9bc27f1ac
mollytyy <moty0519@gmail.com>
2021-04-26 21:16:11 -0500

    ????

--- app/src/main/java/com/example/vasistartapp/settings.java
+++ app/src/main/java/com/example/vasistartapp/settings.java
@@ -8,8 +8,6 @@ import android.widget.Spinner;
 import android.widget.ArrayAdapter;
 import android.widget.Switch;
 
-import java.util.ArrayList;
-import java.util.List;
 
 public class settings extends AppCompatActivity {
 
